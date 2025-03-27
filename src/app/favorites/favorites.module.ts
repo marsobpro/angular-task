2042749b49b1cefbb80b72fd6061854d955dc2f5
Marcin Sobieraj <marsobpro@gmail.com>
2025-03-27 17:03:02 +0100

    Favorites refactor and cleanup

--- src/app/favorites/favorites.module.ts
+++ src/app/favorites/favorites.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { FavoritesComponent } from './components/favorites.component';
 import { FavoritesRoutingModule } from './favorites-routing.module';
-import { CommonModule } from '@angular/common';
 import { YoutubeModule } from '../youtube/youtube.module';
 
 @NgModule({
   declarations: [FavoritesComponent],
   imports: [FavoritesRoutingModule, CommonModule, YoutubeModule],
-  providers: [],
   exports: [FavoritesComponent],
 })
 export class FavoritesModule {}
